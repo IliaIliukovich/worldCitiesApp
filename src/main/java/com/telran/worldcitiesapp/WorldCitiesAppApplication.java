8f7e6813356a79785c7120417eb29876731e83ed
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-05-22 13:47:34 +0200

    initial commit

---
+++ src/main/java/com/telran/worldcitiesapp/WorldCitiesAppApplication.java
@@ -0,0 +1,13 @@
+package com.telran.worldcitiesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCitiesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldCitiesAppApplication.class, args);
+    }
+
+}
