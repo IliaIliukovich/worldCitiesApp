a804fa492df32e9ac63f3b1cca23a855fd664bc8
4rad95 <4rad@i.ua>
2024-06-10 15:11:27 +0200

    CountryController

--- src/main/java/com/telran/worldcitiesapp/controller/CityController.java
+++ src/main/java/com/telran/worldcitiesapp/controller/CityController.java
@@ -4,28 +4,20 @@ import com.telran.worldcitiesapp.model.City;
 import com.telran.worldcitiesapp.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
 public class CityController {
 
-    private final CityService service;
-
     @Autowired
-    public CityController(CityService service) {
-        this.service = service;
-    }
+    private CityService service;
 
     @GetMapping("/allCities")
     public List<City> getAll() {
         return service.getAllCities();
     }
 
-    @GetMapping("/allCitiesByCountry")
-    public List<City> getAllByCountry(@RequestParam(name = "code") String code) {
-        return service.getAllCitiesByCountry(code);
-    }
 
 }
